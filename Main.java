99310a614bb719d8d6b840cec43944ab328e68f9
allen <32278167+allenerocha@users.noreply.github.com>
2019-04-12 07:44:39 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,7 +10,7 @@
 public class Main {
     public static void main(String[] args) {
         // MENU
-        System.out.println("::MENU::\n1. Type\t2. Organize dictionary\n");
+        System.out.println("::MENU::\n1. Type\n2. Organize dictionary\n");
         java.util.Scanner scanner = new java.util.Scanner(System.in);
         int input = scanner.nextInt();
         switch (input) {
